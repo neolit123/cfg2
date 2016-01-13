e8c107438f9346c450a5cb77f1b8a2d67ab81d0f
Lubomir I. Ivanov <neolit123@gmail.com>
2016-01-13 16:51:50 +0200

    fix another typo in the header
    Signed-off-by: Lubomir I. Ivanov <neolit123@gmail.com>

--- include/cfg2.h
+++ include/cfg2.h
@@ -158,7 +158,7 @@ cfg_double cfg_value_get_double(cfg_t *st, cfg_char *key);
 /* set a value for a specific key; add the key if missing. */
 cfg_error_t cfg_value_set(cfg_t *st, cfg_char *key, cfg_char *value, cfg_bool add);
 
-/* set a value for a specific key in a seection; add the key if missing. */
+/* set a value for a specific key in a section; add the key if missing. */
 cfg_error_t cfg_section_value_set(cfg_t *st, cfg_char *section, cfg_char *key, cfg_char *value, cfg_bool add);
 
 /* direct string -> number conversations; same as the ones above */
